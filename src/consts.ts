1c2d9078ded217feb3b555a8c1cf299f7888f307
Darián López Utra <daroxs95@gmail.com>
2023-10-31 19:08:27 -0400

    Bootstrap

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+export const MY_NAME = "Darián López Utra";
+export const SITE_TITLE = "Dominique game engine";
+export const SITE_MADE_IN_YEAR = "2023";
+export const SITE_DESCRIPTION = "An educational first game engine";
+export const GITHUB = "https://github.com/daroxs95/Dominique";
+export const INSTA = "#";
+export const X = "#";
+export const EMAIL = "daroxs95@gmail.com";
